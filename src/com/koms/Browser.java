cca2f2daa1e9bc5da1f5e191f4190fefe7596cf0
Leonid Babikov <komsldt@gmail.com>
2017-01-29 18:17:59 +0300

    updated chromium startup.  added config loader

--- src/com/koms/Browser.java
+++ src/com/koms/Browser.java
@@ -2,16 +2,33 @@ package com.koms;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 /**
  * Created by leonid on 23.01.17.
  */
 public class Browser {
     WebDriver driver;
     public Browser(){
-        System.setProperty("webdriver.chrome.driver", "/home/leonid/phpstorm-workspace/webDriver/chromedriver64");
-        driver = new ChromeDriver();
-        driver.navigate().to("http://ya.ru");
+
+        System.setProperty("webdriver.chrome.driver", ConfigLoader.DriverPath);
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("-incognito");
+        options.addArguments("--user-data-dir="+ConfigLoader.UserDataDir);
+        options.addArguments("--profile-directory=Default");
+        options.setBinary(ConfigLoader.BrowserPath);
+        options.addArguments("load-extension="+ConfigLoader.KC3Path);
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        driver = new ChromeDriver(capabilities);
+
+
         try{
+            Thread.currentThread().sleep(2000);
+            driver.navigate().to("http://ya.ru");
             Thread.currentThread().sleep(10000);
         }catch (Exception e){
             e.printStackTrace();
---
+++ src/com/koms/ConfigLoader.java
@@ -0,0 +1,46 @@
+package com.koms;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileReader;
+
+/**
+ * Created by Leonid on 29.01.2017.
+ */
+public class ConfigLoader {
+    public static String UserDataDir;
+    public static String DriverPath;
+    public static String BrowserPath;
+    public static String KC3Path;
+
+    private class Config{
+        private String UserDataDir;
+        private String DriverPath;
+        private String BrowserPath;
+        private String KC3Path;
+    }
+
+    public ConfigLoader() {
+        loadConfig();
+        System.out.println("udd = " + UserDataDir);
+        System.out.println("dp  = " + DriverPath);
+        System.out.println("bp  = " + BrowserPath);
+        System.out.println("kc3 = " + KC3Path);
+    }
+
+    private void loadConfig() {
+        try {
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader("config.json"));
+            Config c = gson.fromJson(reader,Config.class);
+            BrowserPath=c.BrowserPath;
+            DriverPath=c.DriverPath;
+            UserDataDir=c.UserDataDir;
+            KC3Path=c.KC3Path;
+        } catch (Exception e) {
+            System.out.println("Error while loading json");
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
